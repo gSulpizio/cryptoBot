7c27ee1409c754ceb9c7168a2798eb6c74c740ad
Giustino Sulpizio <ishakgiustino@gmail.com>
2021-05-08 18:09:28 +0200

    fix: edited custom function to reflect new faster version

--- CustomFunctions.py
+++ CustomFunctions.py
@@ -8,27 +8,27 @@ import matplotlib.pyplot as plt
 
 def SMAcalc_binance(begin, days, data_for_SMA, target_column):
     SMA = 0
-    start = begin+days-1
-    stop = begin-1
+    start = begin + days - 1
+    stop = begin - 1
     for i in range(start, stop, -1):
         SMA += data_for_SMA.iloc[i, target_column]
-    SMA = SMA/days
+    SMA = SMA / days
     return SMA
 
 
 def EMAcalc_binance(n, days, data_for_EMA, target_column, prev_EMA):
     # start=begin+days-1
     # stop=begin-1
-    fx = 2/(1+days)
+    fx = 2 / (1 + days)
     EMA_p = prev_EMA
-    EMA = fx*(data_for_EMA.iloc[n, target_column]-EMA_p)+EMA_p
-#    EMA=EMA_p*(1-fx)+data_for_EMA.iloc[n, target_column]*fx
+    EMA = fx * (data_for_EMA.iloc[n, target_column] - EMA_p) + EMA_p
+    #    EMA=EMA_p*(1-fx)+data_for_EMA.iloc[n, target_column]*fx
     return EMA
 
 
 def momentum_calc(data):
-    mom1 = data.iloc[0, 3]/data.iloc[1, 3]*100
-    mom2 = data.iloc[1, 3]/data.iloc[2, 3]*100
+    mom1 = data.iloc[0, 3] / data.iloc[1, 3] * 100
+    mom2 = data.iloc[1, 3] / data.iloc[2, 3] * 100
     mom3 = data.iloc[2, 3] / data.iloc[3, 3] * 100
     mom4 = data.iloc[3, 3] / data.iloc[4, 3] * 100
     return mom1, mom2, mom3, mom4
@@ -45,10 +45,10 @@ def is_time_between(begin_time, end_time, check_time=None):
 
 def momentum_calc_test(day, data):
     day_a = day
-    mom1 = data.iloc[day_a, 3] / data.iloc[day_a+1, 3] * 100
-    mom2 = data.iloc[day_a+1, 3] / data.iloc[day_a+2, 3] * 100
-    mom3 = data.iloc[day_a+2, 3] / data.iloc[day_a+3, 3] * 100
-    mom4 = data.iloc[day_a+3, 3] / data.iloc[day_a+4, 3] * 100
+    mom1 = data.iloc[day_a, 3] / data.iloc[day_a + 1, 3] * 100
+    mom2 = data.iloc[day_a + 1, 3] / data.iloc[day_a + 2, 3] * 100
+    mom3 = data.iloc[day_a + 2, 3] / data.iloc[day_a + 3, 3] * 100
+    mom4 = data.iloc[day_a + 3, 3] / data.iloc[day_a + 4, 3] * 100
     return mom1, mom2, mom3, mom4
 
 
@@ -59,10 +59,17 @@ def buy_conditions(df, n, trade_margin):
     lower_threshold_prev = df.iloc[n + 1, 16]
     df.iloc[n, 18] = upper_threshold * (1 + trade_margin)
 
-    if (lower_threshold < df.iloc[n, 4] and lower_threshold_prev > df.iloc[n + 1, 4] and df.iloc[n + 1, 12] < df.iloc[n + 1, 4]):
+    if (
+        lower_threshold < df.iloc[n, 4]
+        and lower_threshold_prev > df.iloc[n + 1, 4]
+        and df.iloc[n + 1, 12] < df.iloc[n + 1, 4]
+    ):
         return True
     # and df.iloc[n+1,12]<df.iloc[n+1, 4]*(1-trade_margin):                                              #unused since we sell everything
-    elif (upper_threshold * (1 + trade_margin) < df.iloc[n, 4] and upper_threshold_prev * (1 + trade_margin) > df.iloc[n + 1, 4]):
+    elif (
+        upper_threshold * (1 + trade_margin) < df.iloc[n, 4]
+        and upper_threshold_prev * (1 + trade_margin) > df.iloc[n + 1, 4]
+    ):
         return True
     else:
         return False
@@ -82,8 +89,8 @@ def sell_conditions(df, n, trade_margin):
 
 def buy_conditions_EMAmom(df, n, avg_EMA, pricecolumn):
     SMA = SMAcalc_binance(n, avg_EMA, df, pricecolumn)
-    SMA_prev = SMAcalc_binance(n+1, avg_EMA, df, pricecolumn)
-    SMA_prev2 = SMAcalc_binance(n+2, avg_EMA, df, pricecolumn)
+    SMA_prev = SMAcalc_binance(n + 1, avg_EMA, df, pricecolumn)
+    SMA_prev2 = SMAcalc_binance(n + 2, avg_EMA, df, pricecolumn)
     SMA_prev3 = SMAcalc_binance(n + 3, avg_EMA, df, pricecolumn)
     if SMA > SMA_prev and SMA_prev < SMA_prev2 and SMA_prev2 < SMA_prev3:
         return True
@@ -93,8 +100,8 @@ def buy_conditions_EMAmom(df, n, avg_EMA, pricecolumn):
 
 def sell_conditions_EMAmom(df, n, avg_EMA, pricecolumn):
     SMA = SMAcalc_binance(n, avg_EMA, df, pricecolumn)
-    SMA_prev = SMAcalc_binance(n+1, avg_EMA, df, pricecolumn)
-    SMA_prev2 = SMAcalc_binance(n+2, avg_EMA, df, pricecolumn)
+    SMA_prev = SMAcalc_binance(n + 1, avg_EMA, df, pricecolumn)
+    SMA_prev2 = SMAcalc_binance(n + 2, avg_EMA, df, pricecolumn)
     SMA_prev3 = SMAcalc_binance(n + 3, avg_EMA, df, pricecolumn)
     if SMA < SMA_prev and SMA_prev > SMA_prev2:  # and SMA_prev2>SMA_prev3:
         return True
@@ -129,7 +136,7 @@ def sell_conditions_4EMA_1(df, n, EMA8_col, EMA13_col, EMA21_col, EMA55_col):
     EMA13 = df.iloc[n, EMA13_col]
     EMA21 = df.iloc[n, EMA21_col]
     EMA55 = df.iloc[n, EMA55_col]
-    if df.iloc[n+1, 4]*1.02 < df.iloc[n, 4]:
+    if df.iloc[n + 1, 4] * 1.02 < df.iloc[n, 4]:
         return True
     if EMA8 < EMA13 or EMA13 < EMA21 or EMA21 < EMA55:  # and SMA_prev2>SMA_prev3:
         return True
@@ -149,7 +156,7 @@ def sell_conditions_MA(df, n, bprice):
     # and df.iloc[n+1,col_df]>df.iloc[n+2,col_df] and df.iloc[n+2,col_df]>df.iloc[n+3,col_df]:
     if df.iloc[n, 4] > df.iloc[n, 17]:
         return True
-    if df.iloc[n, 4] < 0.99*bprice:
+    if df.iloc[n, 4] < 0.99 * bprice:
         return True
     else:
         return False
@@ -175,53 +182,55 @@ def sell_conditions_RSI(df, n, bprice):
     RSI = df.iloc[n, 12]
     if RSI > 70:
         return True
-    if df.iloc[n, 4] < 0.99*bprice:
+    if df.iloc[n, 4] < 0.99 * bprice:
         return True
     else:
         return False
 
 
-def RSI_SMA(df, n, colprice, rad):  # with simple moving avg. for EMA, change after the while
+def RSI_SMA(
+    df, n, colprice, rad
+):  # with simple moving avg. for EMA, change after the while
     upcounter = 0
     downcounter = 0
     upsum = 0
     downsum = 0
-    n_count = n+rad-1
+    n_count = n + rad - 1
     while n_count >= 0:
-        if df.iloc[n_count, colprice] > df.iloc[n_count+1, colprice]:  # up
+        if df.iloc[n_count, colprice] > df.iloc[n_count + 1, colprice]:  # up
             upcounter += 1
-            upsum += abs(df.iloc[n_count, colprice] -
-                         df.iloc[n_count+1, colprice])
-        elif df.iloc[n_count, colprice] < df.iloc[n_count+1, colprice]:
+            upsum += abs(df.iloc[n_count, colprice] - df.iloc[n_count + 1, colprice])
+        elif df.iloc[n_count, colprice] < df.iloc[n_count + 1, colprice]:
             downcounter += 1
-            downsum += abs(df.iloc[n_count, colprice] -
-                           df.iloc[n_count+1, colprice])
-        n_count = n_count-1
+            downsum += abs(df.iloc[n_count, colprice] - df.iloc[n_count + 1, colprice])
+        n_count = n_count - 1
 
-    avg_up = upsum/rad
-    avg_down = downsum/rad
+    avg_up = upsum / rad
+    avg_down = downsum / rad
 
-    RS = avg_up/avg_down
-    RS_index = 100-100/(1+RS)
+    RS = avg_up / avg_down
+    RS_index = 100 - 100 / (1 + RS)
 
     return RS_index
 
 
 def EMAcalc_for_RSI(days, price, prev_EMA):
-    fx = 2/(1+days)
+    fx = 2 / (1 + days)
     EMA_p = prev_EMA
-    EMA = fx*(price-EMA_p)+EMA_p
+    EMA = fx * (price - EMA_p) + EMA_p
     return EMA
 
 
-def RSI_EMA(df, n, colprice, rad):  # with simple moving avg. for EMA, change after the while
+def RSI_EMA(
+    df, n, colprice, rad
+):  # with simple moving avg. for EMA, change after the while
     import numpy as np
 
     upcounter = 0
     downcounter = 0
     upsum = 0
     downsum = 0
-    n_count = n+rad-1
+    n_count = n + rad - 1
     i = 0
     j = 0
 
@@ -229,25 +238,25 @@ def RSI_EMA(df, n, colprice, rad):  # with simple moving avg. for EMA, change af
     DNS = np.zeros(rad)
 
     while n_count >= n:
-        if df.iloc[n_count, colprice] > df.iloc[n_count+1, colprice]:  # up
-            UPS[i] = abs(df.iloc[n_count, colprice] -
-                         df.iloc[n_count+1, colprice])
+        if df.iloc[n_count, colprice] > df.iloc[n_count + 1, colprice]:  # up
+            UPS[i] = abs(df.iloc[n_count, colprice] - df.iloc[n_count + 1, colprice])
             if upcounter == 0:
-                EMA_up = abs(df.iloc[n_count, colprice] -
-                             df.iloc[n_count+1, colprice])
+                EMA_up = abs(
+                    df.iloc[n_count, colprice] - df.iloc[n_count + 1, colprice]
+                )
             i += 1
             upcounter += 1
 
-        elif df.iloc[n_count, colprice] < df.iloc[n_count-1, colprice]:
-            DNS[j] = abs(df.iloc[n_count, colprice] -
-                         df.iloc[n_count + 1, colprice])
+        elif df.iloc[n_count, colprice] < df.iloc[n_count - 1, colprice]:
+            DNS[j] = abs(df.iloc[n_count, colprice] - df.iloc[n_count + 1, colprice])
             if downcounter == 0:
-                EMA_down = abs(df.iloc[n_count, colprice] -
-                               df.iloc[n_count+1, colprice])
+                EMA_down = abs(
+                    df.iloc[n_count, colprice] - df.iloc[n_count + 1, colprice]
+                )
             downcounter += 1
             j += 1
 
-        n_count = n_count-1
+        n_count = n_count - 1
 
     # deleting 0 values:
     delete_counter = len(UPS) - 1
@@ -256,7 +265,7 @@ def RSI_EMA(df, n, colprice, rad):  # with simple moving avg. for EMA, change af
             UPS = np.delete(UPS, delete_counter)
         delete_counter -= 1
 
-    delete_counter = len(DNS)-1
+    delete_counter = len(DNS) - 1
     while delete_counter >= 0:
         if DNS[delete_counter] == 0:
             DNS = np.delete(DNS, delete_counter)
@@ -275,8 +284,8 @@ def RSI_EMA(df, n, colprice, rad):  # with simple moving avg. for EMA, change af
 
     avg_up = EMA_up
     avg_down = EMA_down
-    RS = avg_up/avg_down
-    RS_index = 100-100/(1+RS)
+    RS = avg_up / avg_down
+    RS_index = 100 - 100 / (1 + RS)
 
     return RS_index
 
@@ -284,18 +293,20 @@ def RSI_EMA(df, n, colprice, rad):  # with simple moving avg. for EMA, change af
 def SMMAcalc_for_RSI(days, gain, prev_avg):
     # fx=1/days
     # EMA=fx*(price-prev_avg)+prev_avg
-    EMA = (prev_avg * (days - 1) + gain)/days
+    EMA = (prev_avg * (days - 1) + gain) / days
     return EMA
 
 
-def RSI_SMMA(df, n, colprice, rad):  # with simple moving avg. for EMA, change after the while
+def RSI_SMMA(
+    df, n, colprice, rad
+):  # with simple moving avg. for EMA, change after the while
     import numpy as np
 
     upcounter = 0
     downcounter = 0
     upsum = 0
     downsum = 0
-    n_count = n+rad-1
+    n_count = n + rad - 1
     i = 0
     j = 0
 
@@ -303,34 +314,34 @@ def RSI_SMMA(df, n, colprice, rad):  # with simple moving avg. for EMA, change a
     DNS = np.zeros(rad)
 
     while n_count >= n:
-        if df.iloc[n_count, colprice] > df.iloc[n_count+1, colprice]:  # up
-            UPS[i] = abs(df.iloc[n_count, colprice] -
-                         df.iloc[n_count+1, colprice])
+        if df.iloc[n_count, colprice] > df.iloc[n_count + 1, colprice]:  # up
+            UPS[i] = abs(df.iloc[n_count, colprice] - df.iloc[n_count + 1, colprice])
             if upcounter == 0:
-                EMA_up = abs(df.iloc[n_count, colprice] -
-                             df.iloc[n_count+1, colprice])
+                EMA_up = abs(
+                    df.iloc[n_count, colprice] - df.iloc[n_count + 1, colprice]
+                )
             i += 1
             upcounter += 1
 
-        elif df.iloc[n_count, colprice] < df.iloc[n_count-1, colprice]:
-            DNS[j] = abs(df.iloc[n_count, colprice] -
-                         df.iloc[n_count + 1, colprice])
+        elif df.iloc[n_count, colprice] < df.iloc[n_count - 1, colprice]:
+            DNS[j] = abs(df.iloc[n_count, colprice] - df.iloc[n_count + 1, colprice])
             if downcounter == 0:
-                EMA_down = abs(df.iloc[n_count, colprice] -
-                               df.iloc[n_count+1, colprice])
+                EMA_down = abs(
+                    df.iloc[n_count, colprice] - df.iloc[n_count + 1, colprice]
+                )
             downcounter += 1
             j += 1
 
-        n_count = n_count-1
+        n_count = n_count - 1
 
     # deleting 0 values:
-    delete_counter = len(UPS)-1
+    delete_counter = len(UPS) - 1
     while delete_counter >= 0:
         if UPS[delete_counter] == 0:
             UPS = np.delete(UPS, delete_counter)
         delete_counter -= 1
 
-    delete_counter = len(DNS)-1
+    delete_counter = len(DNS) - 1
     while delete_counter >= 0:
         if DNS[delete_counter] == 0:
             DNS = np.delete(DNS, delete_counter)
@@ -357,8 +368,8 @@ def RSI_SMMA(df, n, colprice, rad):  # with simple moving avg. for EMA, change a
         EMA_down = SMMAcalc_for_RSI(rad, DNS[EMA_counter], EMA_down)
         EMA_counter += 1
 
-    RS = EMA_up/EMA_down
-    RS_index = 100-100/(1+RS)
+    RS = EMA_up / EMA_down
+    RS_index = 100 - 100 / (1 + RS)
 
     return RS_index
 
@@ -404,23 +415,24 @@ def sell_conditions_EMA_f(df, n):
     else:
         return False
 
+
 def buy_conditions_SMA(df, n):
-    EMA15 = df['EMA15'][n]
-    SMA90 = df['SMA90'][n]
-    pEMA15 = df['EMA15'][n-1]
-    pSMA90 = df['SMA90'][n-1]
-    if EMA15>SMA90 and pEMA15<pSMA90:
+    EMA15 = df["EMA15"][n]
+    SMA90 = df["SMA20"][n]
+    pEMA15 = df["EMA15"][n - 1]
+    pSMA90 = df["SMA20"][n - 1]
+    if EMA15 > SMA90 and pEMA15 < pSMA90:
         return True
     else:
         return False
 
 
 def sell_conditions_SMA(df, n):
-    EMA15 = df['EMA15'][n]
-    SMA90 = df['SMA90'][n]
-    pEMA15 = df['EMA15'][n-1]
-    pSMA90 = df['SMA90'][n-1]
-    if EMA15<SMA90 and pEMA15>pSMA90:
+    EMA15 = df["EMA15"][n]
+    SMA90 = df["SMA20"][n]
+    pEMA15 = df["EMA15"][n - 1]
+    pSMA90 = df["SMA20"][n - 1]
+    if EMA15 < SMA90 and pEMA15 > pSMA90:
         return True
     else:
         return False
