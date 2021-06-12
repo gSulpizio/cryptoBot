a13cf90f51b59f7d45ef658ef0f646919a39318f
Giustino Sulpizio <ishakgiustino@gmail.com>
2021-06-12 15:42:26 +0200

    fix: buy/sell error handling

--- binance_orders.py
+++ binance_orders.py
@@ -9,13 +9,23 @@ client = Client(BKeys.key(), BKeys.SecretKey())
 
 def sell_order(pair, quantity):
     counter = 0
-    order = client.order_market(symbol=pair, quantity=quantity, side="SELL")
-    status = order["status"]
+    try:
+        order = client.order_market(symbol=pair, quantity=quantity, side="SELL")
+        status = order["status"]
+        if status == "FILLED":
+            return
+    except:
+        psh.push("Sell order raised error, initiating retry")
     while True:
-        if status != "FILLED" and counter < 10:
-            tm.sleep(15)
-            order = client.order_market(symbol=pair, quantity=quantity, side="SELL")
-            status = order["status"]
+        if counter < 10:
+            tm.sleep(60)
+            try:
+                order = client.order_market(symbol=pair, quantity=quantity, side="SELL")
+                status = order["status"]
+                if status == "FILLED":
+                    return
+            except:
+                psh.push(f"try {counter}, error")
         elif counter == 10:
             psh.push("SELL ORDER COULD NOT BE PLACED")
         else:
@@ -24,13 +34,24 @@ def sell_order(pair, quantity):
 
 def buy_order(pair, quantity):
     counter = 0
-    order = client.order_market(symbol=pair, quantity=quantity, side="BUY")
+    try:
+        order = client.order_market(symbol=pair, quantity=quantity, side="BUY")
+        status = order["status"]
+        if status == "FILLED":
+            return
+    except:
+        psh.push("Buy order raised error, initiating retry")
     status = order["status"]
     while True:
-        if status != "FILLED" and counter < 10:
-            tm.sleep(15)
-            order = client.order_market(symbol=pair, quantity=quantity, side="BUY")
-            status = order["status"]
+        if counter < 10:
+            tm.sleep(60)
+            try:
+                order = client.order_market(symbol=pair, quantity=quantity, side="BUY")
+                status = order["status"]
+                if status == "FILLED":
+                    return
+            except:
+                psh.push(f"try {counter}, error")
         elif counter == 10:
             psh.push("BUY ORDER COULD NOT BE PLACED")
         else:
