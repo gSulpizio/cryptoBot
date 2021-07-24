0510c2b3cc97353e272270c7ca104555adad25a9
Giustino Sulpizio <ishakgiustino@gmail.com>
2021-07-24 17:36:16 +0200

    feat: initialize.py now initializes the dataframe with the 500 previous data points

--- constants.py
+++ constants.py
@@ -4,3 +4,8 @@ def interval():
     return "1h"
 def rounding():
     return 4
+
+if __name__ == "__main__":
+    print(symbol())
+    print(interval())
+    print(rounding())
--- main_function.py
+++ main_function.py
@@ -1,2 +1,3 @@
 from constants import *
 
+#implement a function that waits until a full hour has passed
