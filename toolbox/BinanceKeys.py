c97921d4a88b6cc940450909217082a78d294f33
Giustino Sulpizio <ishakgiustino@gmail.com>
2021-07-26 22:46:28 +0200

    chore: documentation

--- toolbox/BinanceKeys.py
+++ toolbox/BinanceKeys.py
@@ -1,7 +1,13 @@
 def key():
+    '''
+    Returns public api key for binance
+    '''
     a='JzrKFJZZeg274N9uTMTRdfqtruipvIE2M6rR1ohYBcVcw2lYVA6d5Q0jVg8asTSC'
     return a
 
 def SecretKey():
+    '''
+    Returns private api key for binance
+    '''
     b='5tHCL85UtoGwLeX13tyICx6lj2C4dBO2Klw1JkISzgHn1k3LQCi55IPcmHKF9ZiA'
     return b
