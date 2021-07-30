71c7b06e75ac19cd191ffc4f4c38ff1e658e6d26
Giustino Sulpizio <ishakgiustino@gmail.com>
2021-07-30 11:48:36 +0200

    fix: debugging

--- crypto_bot.py
+++ crypto_bot.py
@@ -24,11 +24,11 @@ while True:
         continue
     df=add_row(df)
     amt1, amt2=balances(df)
-    if amt2>amt1 and buy_condition():
+    if amt2>amt1 and buy_condition(df):
         binance_order(symbol(), amt2, "BUY")
         push(f"BUY, price: {df['price'][len(df)-1]}, @: {datetime.now()}")
         bought_price=df['price'][len(df)-1]
-    if amt1>amt2 and sell_condition():
+    if amt1>amt2 and sell_condition(df):
         binance_order(symbol(), amt2, "BUY")
         rtrn=round(df['price'][len(df)-1]/bought_price, 2)
         push(f"BUY, price: {df['price'][len(df)-1]}, return: {rtrn}% @:',{datetime.now()}")
